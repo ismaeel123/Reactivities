86e8c96402789ef7082bdcc27965e80226cdc04a
ismaeel123 <37120315+ismaeel123@users.noreply.github.com>
2021-07-21 23:35:56 +0200

    End of Section 4

---
+++ Application/Activities/List.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistance;
+
+namespace Application.Activities
+{
+    public class List
+    {
+        public class Query : IRequest<List<activity>> { }
+
+        public class Handler : IRequestHandler<Query, List<activity>>
+        {
+            private readonly DataContext _context;
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+            public async Task<List<activity>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                return await _context.Activites.ToListAsync();
+            }
+        }
+    }
+}
